9262038f3f6a2c4d3ffc997e2e72b52aee8daaa1
eponce5 <eponce5@mail.valenciacollege.edu>
2021-02-13 20:57:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+console.log("I am having a lot of fun with this class, it is hard and easy at the same time");
+
+let nameStatement = "My name is "
+nameStatement= nameStatement + "Ekaterina"
+
+
+const Age= 19;
+
+alert(nameStatement)
+alert("I am " + 19 + " years old");
